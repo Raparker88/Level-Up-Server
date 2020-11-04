fb6726bc0ac70cefefcea8851ceb536cde2d31d8
Raparker88 <raparker88@gmail.com>
2020-11-04 14:41:26 -0600

    added custom action and unmapped property to events for gamers to register to attend events

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -10,3 +10,11 @@ class Event(models.Model):
     description = models.CharField(max_length=200)
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     location = models.CharField(max_length=100)
+
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
